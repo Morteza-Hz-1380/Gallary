cf38b52091bc818df96655e12fcaf603131f6716
Morteza <mortezahossainzade11@gmail.com>
2024-01-15 10:57:34 +0430

    Adding opacity

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,9 +1,28 @@
+import './Photo.css';
+
 const Photo = ({ photo }) => {
+  const handleDownload = () => {
+    // Fetch the image data
+    fetch(photo.src.original)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a download link
+        const downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.download = 'photo'; // You can set a default filename here or use the photo's name
+        downloadLink.click();
+      });
+  };
+
   return (
-    <div >
-      <img src={photo.src.large} alt={photo.photographer} />
-      {photo.photographer}
-    </div>
+    <>
+      <span className="opacity">
+        <h3>{photo.photographer}</h3>
+        <button onClick={handleDownload}>Download</button>
+        <img src={photo.src.large} alt={photo.photographer} />
+      </span>
+    </>
   );
 };
+
 export default Photo;
